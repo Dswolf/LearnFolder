60d7dc27260a616c00029462bbe1f316d5b7f16a
GameWolf <seraphdream0521@gmail.com>
2018-12-20 11:44:52 +0800

    Test2

--- dict_custom-1.js
+++ dict_custom-1.js
@@ -1,6 +1,28 @@
+// Js Read-------------------------
+document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>');
+
+var $show = $('#show');
+$.ajax({
+    type: "post",
+    data: {
+        "method": "read",
+        "query": "New",
+    },
+    // 填入網路應用程式網址
+    url: "https://script.google.com/macros/s/AKfycbxBcSeU_pTMYwJPcGvW5zN_lo01GFD0trsq_mSnNuV1Hs6BozU/exec", // 填入網路應用程式網址
+    success: function (e) {
+        //alert(e);
+        $show.text('@' + e + '@');
+    }
+});
+
+// Js Read End-------------------------
+
 _custom_dict = [
     ["可以開始開始", 15, "n"],
 ];
+
+
 
 // 引用設定檔案，以下不用變更
 if (typeof (define) === "function") {
